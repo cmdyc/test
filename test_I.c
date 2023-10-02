af5b995c0a95f5035f0a39d4fe74b71dc6506def
聪明的洋葱 <144433648+cmdyc@users.noreply.github.com>
2023-10-02 21:56:02 +0800

    a small file

---
+++ test_I.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+    int num ;
+    num = 1 ;
+
+    printf(" i am a simple ") ;
+    printf(" computer.\n ") ;
+    printf(" my favorite number is %d because it is first.\n ",num ) ;
+    
+    getchar();
+
+    return 0 ;
+}
